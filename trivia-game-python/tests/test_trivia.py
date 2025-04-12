e5258a008fe956bdb108d2ee84bdd9987f4bfc30
Chriss5-2 <chris.luna.j@uni.pe>
2025-04-12 13:20:44 -0500

    Add test_quiz_scoring method in test_trivia.py

--- trivia-game-python/tests/test_trivia.py
+++ trivia-game-python/tests/test_trivia.py
@@ -1,5 +1,5 @@
 import pytest
-from app.trivia import Question
+from app.trivia import Quiz, Question
 
 def test_question_correct_answer():
     question = Question("¿Qué juego ganó el GOTY el año 2024?", ["Astro Bot", "God of War Ragnarok", "The Last of Us 2", "Ghost of Tsushima"], "1")
@@ -20,3 +20,10 @@ def test_question_incorrect_type_input():
 def test_question_empty_answer():
     question = Question("¿Qué juego ganó el GOTY el año 2024?", ["Astro Bot", "God of War Ragnarok", "The Last of Us 2", "Ghost of Tsushima"], "1")
     assert not question.is_correct("")
+
+def test_quiz_scoring():
+    quiz = Quiz()
+    question = Question("¿Qué juego ganó el GOTY el año 2024?", ["Astro Bot", "God of War Ragnarok", "The Last of Us 2", "Ghost of Tsushima"], "1")
+    quiz.add_question(question)
+    assert quiz.asnwer_question(question, "1") == True
+    assert quiz.correct_answers == 1
