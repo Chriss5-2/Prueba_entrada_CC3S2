86c1eb829001641533d7769c0b7538b7f89c6775
Chriss5-2 <chris.luna.j@uni.pe>
2025-04-15 10:07:35 -0500

    Add post in main.py

--- trivia-game-python/app/main.py
+++ trivia-game-python/app/main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import List
 
 app = FastAPI()
+
+class Question(BaseModel):
+    description: str
+    options: List[str]
+    correct_answer: str
 
 @app.get("/")
 def read_root():
     return {"message": "Trivia Game API is running!"}
+
+@app.post("/questions/", status_code=201)
+def create_question(question: Question):
+    return {"message": "Question created", "question":question}
